082ffb59efbc4513c769dbee9cfe9bec75f785d0
Kirillmukh <Kirillmukh@mail.ru>
2025-03-25 17:34:52 +0300

    Initial commit

---
+++ BackendConsultantInternship/src/main/java/com/example/consultantinternship/ConsultantInternshipApplication.java
@@ -0,0 +1,13 @@
+package com.example.consultantinternship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultantInternshipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsultantInternshipApplication.class, args);
+    }
+
+}
---
+++ BackendConsultantInternship/src/test/java/com/example/consultantinternship/ConsultantInternshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.consultantinternship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultantInternshipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
