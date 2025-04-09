0424a9c727e4fd419e3862c64548f710abbd9816
kirillmukh <kirillmukh@mail.ru>
2025-04-09 19:37:05 +0300

    quick fix for server

--- BackendConsultantInternship/src/main/java/com/example/consultantinternship/config/OpenApiBeans.java
+++ BackendConsultantInternship/src/main/java/com/example/consultantinternship/config/OpenApiBeans.java
@@ -3,6 +3,7 @@ package com.example.consultantinternship.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,10 +11,12 @@ import java.util.List;
 
 @Configuration
 public class OpenApiBeans {
+    @Value("${server.port}")
+    private String port;
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
-                .servers(List.of(new Server().url("http://localhost:8080")))
+                .servers(List.of(new Server().url("http://localhost:%s".formatted(port))))
                 .info(new Info().title("ConsultantInternship API"));
     }
 }
