658090c2166c00dd2270a3da2d5859da1eff1d03
DNScookie <dnscookie@gmail.com>
2025-04-10 10:32:20 +0300

    infinite update fix

--- FrontendConsultantInternship/smart-franchize-website/vue.config.js
+++ FrontendConsultantInternship/smart-franchize-website/vue.config.js
@@ -1,7 +1,10 @@
 module.exports = {
   devServer: {
     host: '0.0.0.0',
-    port: 5173, // или любой свободный порт
-    allowedHosts: 'all', // важно, если используешь Vue 3 / Vite
+    port: 5173,
+    allowedHosts: 'all',
+    client: {
+      webSocketURL: 'ws://138.124.51.90:5173/ws', // замени на IP или домен сервера
+    },
   }
 }
