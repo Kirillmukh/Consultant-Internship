6868a5ca379c95340cfaf860a8dfe385e7499d46
kirillmukh <kirillmukh@mail.ru>
2025-04-11 13:23:47 +0300

    add summary

--- BackendConsultantInternship/src/main/java/com/example/consultantinternship/service/DefaultRiskService.java
+++ BackendConsultantInternship/src/main/java/com/example/consultantinternship/service/DefaultRiskService.java
@@ -67,8 +67,29 @@ public class DefaultRiskService implements RiskService {
         }
         resultRisks.sort(resultRiskComparator);
 
+        String summary = this.getSummary(riskCount);
+
         resultRisksResponse.setRiskCount(riskCount);
         resultRisksResponse.setData(resultRisks);
+        resultRisksResponse.setSummary(summary);
         return resultRisksResponse;
+    }
+
+    private String getSummary(Map<String, Integer> riskCount) {
+        int highCount = riskCount.getOrDefault("Высокий", 0);
+        int middleCount = riskCount.getOrDefault("Средний", 0);
+        int lowCount = riskCount.getOrDefault("Низкий", 0);
+
+        if (highCount > 2) {
+            return "В Вашем договоре было выявлено большое количество условий с высоким риском, что может повлечь неблагоприятные для Вас последствия. Рекомендуем обратить особе внимание на эти условия.";
+        }
+        if (highCount != 0) {
+            return "В Вашем договоре были выявлены условия с высоким риском, что может повлечь неблагоприятные для Вас последствия. Рекомендуем обратить особе внимание на эти условия.";
+        }
+        if (middleCount > (lowCount + highCount) / 2) {
+            return "В Вашем договоре не выявлено условий с высоким риском, однако ряд условий влечет возможность возникновения среднего уровня риска. Рекомендуем обратить особе внимание на эти условия.";
+        }
+
+        return "В вашем договоре не выявлены условия, которые могут повлечь неблагоприятный для Вас последствия. Рекомендуем ознакомиться с пояснениями по каждому вопросу";
     }
 }
